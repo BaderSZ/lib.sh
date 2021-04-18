57ed24ef40338456d27f0217bfa5b2c260e62ffa
Bader Zaidan <bzaidan@studentsforliberty.org>
2021-04-18 13:53:05 +0200

    Add complete lib.sh inclusion

---
+++ lib.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+#set -euo pipefail
+
+
+source src/assert.sh
+source src/complex.sh
+source src/ctype.sh
+source src/errno.sh
+source src/fenv.sh
+source src/float.sh
+source src/inttypes.sh
+source src/iso646.sh
+source src/limits.sh
+source src/locale.sh
+source src/math.sh
+source src/setjmp.sh
+source src/signal.sh
+source src/stdalign.sh
+source src/stdarg.sh
+source src/stdbool.sh
+source src/stddef.sh
+source src/stdint.sh
+source src/stdio.sh
+source src/stdlib.sh
+source src/stdnoreturn.sh
+source src/string.sh
+source src/tgmath.sh
+source src/time.h
+source src/uchar.h
+source src/wchar.h
+source src/wctype.h
