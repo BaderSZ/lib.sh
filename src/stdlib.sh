6d21e060171953a4623b0ccf6303cc1e823ae5eb
Bader Zaidan <bzaidan@studentsforliberty.org>
2021-04-18 13:43:22 +0200

    Add stdlib.sh

---
+++ src/stdlib.sh
@@ -0,0 +1,50 @@
+#!/bin/env bash
+
+# Copyright (C) 2021 Bader Zaidan
+
+NULL=0
+
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
+
+RAND_MAX=32767
+MB_CUR_MAX=128
+
+atof() {
+  RETVAL=$(printf "%f" \'${1})
+}
+
+atoi() {
+  RETVAL=$(printf "%i" \'${1})
+}
+
+atol() {
+  RETVAL=$(printf "%ld" \'${1})
+}
+
+abort() {
+  exit ${EXIT_FAILURE}
+}
+
+# exit() already defined in Bash
+
+getenv() {
+  RETVAL=$(echo "${1}")
+}
+
+abs() {
+  RETVAL=$(echo "${1#-}")
+}
+
+div() {
+  RETVAL=$(echo "scale=2 ; ${1} / ${2}" | bc)
+}
+
+rand() {
+  RETVAL=${RANDOM}
+}
+
+mblen() {
+  RETVAL=${#1}
+}
+
