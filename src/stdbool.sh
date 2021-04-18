6aeedcc55d921fc35fca39b7201e2038b7b3d5f4
Bader Zaidan <bzaidan@studentsforliberty.org>
2021-04-18 14:33:33 +0200

    Add error checking to stdbool

--- src/stdbool.sh
+++ src/stdbool.sh
@@ -1,11 +1,24 @@
 #!/bin/env bash
 
+source stdlib.sh
+
 # Copyright (C) 2021 Bader Zaidan
 
 true=1
 false=0
 
 bool() {
+  if [[ $# != 2 ]]
+  then
+    echo "Incorrect number of arguments: Need 2, $# passed."
+     return ${EXIT_FAILURE}
+  fi
+
+  if [[ ! $2 =~ ^(true|false|1|0)$ ]]
+  then
+    echo "Invalid bool value: Accepting true|false|1|0"
+  fi
+
   eval ${1}=${!2}
 }
 
